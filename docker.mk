93475fbd925fe887cd997433e3d56f5ed177220c
Daniil Akhmerov <52052687+daakhmerov@users.noreply.github.com>
2024-12-09 17:34:52 +0300

    Update docker.mk

--- docker.mk
+++ docker.mk
@@ -1,23 +1,20 @@
 DOCKERHOST := detcorpus
+ip_addr := 10.0.0.19
 noskeimage := maslinych/noske-alt:2.142-alt1
 localarch := export
 remoteroot := corpora
 remotearch := setup
 corplist = $(corpora) 
 configfiles := $(patsubst %,config/%,$(corplist)) 
-corpvertfiles := $(patsubst %,%.vert,$(corplist))
+corpvertfiles := $(wildcard $(localarch)/vert/*.vert)
 subcfiles := config/detcorpus-fiction.subcorpora
 archfile := detcorpus.tar.xz 
-exportfiles := $(patsubst config/%,$(localarch)/registry/%,$(configfiles) $(subcfiles)) $(patsubst %,$(localarch)/vert/%,$(corpvertfiles))
+exportfiles := $(patsubst config/%,$(localarch)/registry/%,$(configfiles) $(subcfiles)) $(corpvertfiles)
 exportdirs := $(patsubst %,$(localarch)/%,registry vert)
 packed := $(localarch)/$(archfile)
 
 
 $(localarch)/registry/% : config/%
-	test -d $(@D) || mkdir -p $(@D)
-	cp -f $< $@
-
-$(localarch)/vert/% : %
 	test -d $(@D) || mkdir -p $(@D)
 	cp -f $< $@
 
@@ -27,15 +24,11 @@ pull-image:
 docker-cleanup: pull-image
 	test -z "$(shell docker ps -aq -f status=exited -f name=$(corpsite))" || @echo docker rm $(corpsite)
 
-docker-local: $(exportfiles) | docker-cleanup
-	if [ ! "$(shell docker ps -a -q -f name=$(corpsite))" ]; then \
-	docker run -dit --name $(corpsite) -v $$(pwd)/$(localarch)/vert:/var/lib/manatee/vert -v $$(pwd)/$(localarch)/registry:/var/lib/manatee/registry -p 127.0.0.1:8088:8080 -e CORPLIST="$(corplist)" $(noskeimage) ;\
-	else \
-	echo "Detcorpus is already running in a local docker container. Stop it first to run anew" ;\
-	fi
+deploy-local: $(exportfiles) | docker-cleanup
+	docker run -dit --name $(corpsite) -v $$(pwd)/$(localarch)/vert:/var/lib/manatee/vert -v $$(pwd)/$(localarch)/registry:/var/lib/manatee/registry -p 127.0.0.1:8088:8080 -e CORPLIST="$(corplist)" $(noskeimage)
 
 deploy: $(exportfiles) | docker-cleanup
-	docker run -dit --name $(corpsite) -v $$(pwd)/$(localarch)/vert:/var/lib/manatee/vert -v $$(pwd)/$(localarch)/registry:/var/lib/manatee/registry -p 10.0.0.19:8098:8080 -e CORPLIST="$(corplist)" $(noskeimage)
+	docker run -dit --name $(corpsite) -v $$(pwd)/$(localarch)/vert:/var/lib/manatee/vert -v $$(pwd)/$(localarch)/registry:/var/lib/manatee/registry -p $(ip_addr):8098:8080 -e CORPLIST="$(corplist)" $(noskeimage)
 
 $(packed) : $(exportfiles)
 	rm -f $@
@@ -53,4 +46,3 @@ remove-testing-docker:
 
 create-testing-docker: 
 	ssh $(DOCKERHOST) 'docker run -dit --name testing -v $$(pwd)/$(remoteroot)/vert:/var/lib/manatee/vert -v $$(pwd)/$(remoteroot)/registry:/var/lib/manatee/registry -p 127.0.0.1:8088:8080 -e CORPLIST="$(corplist)" $(noskeimage)'
-
